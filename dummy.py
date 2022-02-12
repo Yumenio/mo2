ea5dcf2c3354626178906c5561326c70d690da06
Yumenio <alejandroandy447@gmail.com>
2022-02-12 17:52:32 -0500

    minor corrections to the backtracking algorithm, just to comment it and never use it again, probably

--- dummy.py
+++ dummy.py
@@ -14,20 +14,15 @@ import numdifftools as nd
 
 
 
-def backtrack(x0, dfx1, dfx2, t, alpha, beta, count):
-    while (f(x0) - (f(x0 - t*np.array([dfx1(x0), dfx2(x0)])) + alpha * t * np.dot(np.array([dfx1(x0), dfx2(x0)]), np.array([dfx1(x0), dfx2(x0)])))) < 0:
-        t *= beta
-#         print("""
-
-# ########################
-# ###   iteration {}   ###
-# ########################
-# """.format(count))
-#         print("Inequality: ",  f(x0) - (f(x0 - t*np.array([dfx1(x0), dfx2(x0)])) + alpha * t * np.dot(np.array([dfx1(x0), dfx2(x0)]), np.array([dfx1(x0), dfx2(x0)]))))
-#         count += 1
-    return t
 
 # backtracking, no filduin
+
+# def backtrack(x0, f, dfx1, dfx2, t, alpha, beta, count):
+#     while (f(x0) - (f(x0 - t*np.array([dfx1(x0), dfx2(x0)])) + alpha * t * np.dot(np.array([dfx1(x0), dfx2(x0)]), np.array([dfx1(x0), dfx2(x0)])))) < 0:
+#         t *= beta
+#     return t
+
+
 # alpha = 0.3
 # beta = 0.8
 
@@ -45,7 +40,7 @@ def backtrack(x0, dfx1, dfx2, t, alpha, beta, count):
 # # dx0 = np.array([.1, 0.05])
 
 # for k in range(100):
-  # t = backtrack(x0, dfx1, dfx2, t, alpha, beta, count)
+  # t = backtrack(x0, f, dfx1, dfx2, t, alpha, beta, count)
   # x0 = np.array([i + t for i in x0])
   # print('iteration', k, 'x,y=',x0)
 
